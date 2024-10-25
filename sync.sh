5d25602fc14b7f3a7889e55ba2f45340ec9e3bff
fcy <devcloud@fcy.zj.cn>
2024-10-26 01:00:15 +0800

    fix: image path

--- sync.sh
+++ sync.sh
@@ -3,9 +3,9 @@ cat image.txt
 while read -r line; do
     echo "Start sync image $line" >&2
     # 使用正则表达式提取两部分内容
-    if [[ $line =~ (.*)/(.*) ]]; then
-        part1=${BASH_REMATCH[1]}
-        part2=${BASH_REMATCH[2]}
+    if [[ $line =~ (.*)/(.*)/(.*) ]]; then
+        part1=${BASH_REMATCH[2]}
+        part2=${BASH_REMATCH[3]}
         # 转换为目标镜像名称
         target_image="registry.cn-hangzhou.aliyuncs.com/spencerswagger/${part1}-${part2}"
         # 执行命令
